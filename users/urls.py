22c33bc96144071475bb853ff31197b5e834000a
Dmytro Radchenko <radchenko.dim@gmail.com>
2024-12-26 15:08:13 +0200

    added profile page

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('hello/', views.hello, name='hello'),
+    path('profile/', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     return render(request, "users/test.html")
+
+def profile(request):
+    return render(request, "users/profile.html")

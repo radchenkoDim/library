d3833b1029d2300ba16de08b9ba91c3710af827f
Dmytro Radchenko <radchenko.dim@gmail.com>
2024-12-29 02:22:13 +0200

    added the ability to add new users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path, include
+from .views import Register
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('hello/', views.hello, name='hello'),
     path('profile/', views.profile, name='profile'),
+    path('register/', Register.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from users.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def hello(request):
@@ -6,3 +10,27 @@ def hello(request):
 
 def profile(request):
     return render(request, "users/profile.html")
+
+class Register(View):
+
+    template_names = 'registration/register.html'
+
+    def get(self, request):
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.template_names, context)
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('login')
+        context = {
+            'form': form
+        }
+        return render(request, self.template_names, context)

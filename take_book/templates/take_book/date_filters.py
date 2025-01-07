c7d0361885ba180642c35f4dd9f2aff691344ada
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-01-07 05:33:35 +0200

    deleted date_filters.py

--- take_book/templates/take_book/date_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-
-register = template.Library()
-
-@register.filter
-def add_months(date, months):
-    return date + relativedelta(months=months)

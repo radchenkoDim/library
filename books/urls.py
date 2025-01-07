d15ebc2f1bc59c3d0ba75688518ff94a84ec863d
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-01-07 23:50:00 +0200

    M link

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("table/", views.table, name="table"),
-    path("how_take/", views.how_take, name="how_take"),
     path("table/<int:book_num>/", views.book, name="book"),
 ]

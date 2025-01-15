fd72a47110574c6557aa084b5984c08012aacafc
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-01-15 03:24:24 +0200

    Added psql for prod

--- maj_lib/settings_prod.py
+++ maj_lib/settings_prod.py
@@ -1,1 +1,13 @@
 from .settings_base import *
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"],
+    }
+}

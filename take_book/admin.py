c75144095601557c8622ce0442b6cf1c31567a38
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-01-16 10:56:13 +0200

    Added want_book form and page in develop branch

--- take_book/admin.py
+++ take_book/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import TakingBook
+from .models import TakingBook, WantBook
 
 # Register your models here.
 @admin.register(TakingBook)
 class TakingBookAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'take_date', 'return_date',)
+    list_filter = ('user', )
+
+
+@admin.register(WantBook)
+class WantBookAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'author', 'publisher', 'date', 'where',)
     list_filter = ('user', )

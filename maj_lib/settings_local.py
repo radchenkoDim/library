e6177bff7d8f274d7a6d35f49b2014d7d7a6c30b
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-01-25 20:02:30 +0200

    prod email settings

--- maj_lib/settings_local.py
+++ maj_lib/settings_local.py
@@ -1,1 +1,17 @@
 from .settings_base import *
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 8025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'default_from_email@email.com'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
--- maj_lib/settings_prod.py
+++ maj_lib/settings_prod.py
@@ -11,3 +11,16 @@ DATABASES = {
         "PORT": os.environ["PGPORT"],
     }
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
+# import certifi, os
+# os.environ['SSL_CERT_FILE'] = certifi.where()

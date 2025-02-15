997b3d9dd9937a2aeaa459ca481d4e04511c85b7
Dmytro Radchenko <radchenko.dim@gmail.com>
2025-02-15 04:34:26 +0200

    table styles

--- static/js/books/script.js
+++ static/js/books/script.js
@@ -1,39 +1,26 @@
 // const data={{ books_json|safe }}; // Виведе дані книг у форматі JSON з Django
 
-document.addEventListener('DOMContentLoaded', function() {
-    const booksDataElement = document.getElementById('books-data');
-    // if (booksDataElement) {
-    //     console.log(data[1]);  // Виведе "Book 1"
-    //     console.log(data[0]['fields']['title']);  // Виведе "Book 1"
-    //     console.log(data[0].fields.title);  // Виведе "Book 1"
-    // } else {
-    //     console.error('Елемент з id "books-data" не знайдений');
-    // }
-});
-
 const tableBody = document.querySelector('tbody');
 const filterInput = document.getElementById('filterInput');
 const sortButton = document.getElementById('sortButton');
 
 let isDescending = true;
 
-// Функція для підсвічування
 function highlight(text, query) {
     if (typeof text !== 'string') {
-        // console.error('Помилка: text не є рядком:', text);
-        return text.toString(); // Повертаємо незмінене значення
+
+        return text.toString();
     }
-    if (!query) return text; // Якщо немає пошукового запиту, повертаємо текст без змін
-    const regex = new RegExp(`(${query})`, 'gi'); // Регулярний вираз для пошуку
-    return text.replace(regex, '<span class="highlight">$1</span>'); // Обгортаємо знайдене в <span>
+    if (!query) return text; 
+    const regex = new RegExp(`(${query})`, 'gi'); 
+    return text.replace(regex, '<span class="highlight">$1</span>'); 
 }
 
 // Функція для рендерингу таблиці з підсвічуванням
 function renderTable(filteredBooks, query) {
-    tableBody.innerHTML = ''; // Очищаємо таблицю
+    tableBody.innerHTML = '';
 
     if (filteredBooks.length === 0) {
-        // Відображаємо повідомлення, якщо нічого не знайдено
         const row = document.createElement('tr');
         row.innerHTML = '<td colspan="4">Нічого не знайдено</td>';
         tableBody.appendChild(row);
@@ -41,7 +28,7 @@ function renderTable(filteredBooks, query) {
     }
 
     filteredBooks.forEach(book => {
-        const bookf = book['fields']; // Дістаємо дані книги
+        const bookf = book['fields'];
         const row = document.createElement('tr');
         row.innerHTML = `
             <td><a href="/books/table/${bookf['num']}">${highlight(bookf['num'].toString(), query)}</a></td>
@@ -73,23 +60,23 @@ filterInput.addEventListener('input', () => {
     renderTable(filteredBooks, filterValue);
 });
 
-sortButton.addEventListener('click', () => {
-    const sortedBooks = [...data].sort((a, b) => {
-        if (isDescending) {
-            return b['fields']['num'] - a['fields']['num']; // Сортування за спаданням
-        } else {
-            return a['fields']['num'] - b['fields']['num']; // Сортування за зростанням
-        }
-    });
+// sortButton.addEventListener('click', () => {
+//     const sortedBooks = [...data].sort((a, b) => {
+//         if (isDescending) {
+//             return b['fields']['num'] - a['fields']['num']; // Сортування за спаданням
+//         } else {
+//             return a['fields']['num'] - b['fields']['num']; // Сортування за зростанням
+//         }
+//     });
     
-    isDescending = !isDescending; // Змінюємо порядок сортування
+//     isDescending = !isDescending; // Змінюємо порядок сортування
 
-    const filterValue = filterInput.value.toLowerCase();
-    renderTable(sortedBooks.filter(book => 
-        book['fields']['num'].toString().includes(filterValue) ||
-        book['fields']['title'].toLowerCase().includes(filterValue) ||
-        book['fields']['author'].toLowerCase().includes(filterValue) ||
-        book['fields']['category'].toLowerCase().includes(filterValue) 
-    ), filterValue);
-});
+//     const filterValue = filterInput.value.toLowerCase();
+//     renderTable(sortedBooks.filter(book => 
+//         book['fields']['num'].toString().includes(filterValue) ||
+//         book['fields']['title'].toLowerCase().includes(filterValue) ||
+//         book['fields']['author'].toLowerCase().includes(filterValue) ||
+//         book['fields']['category'].toLowerCase().includes(filterValue) 
+//     ), filterValue);
+// });
 
